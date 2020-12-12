33fb0ffd5a5e042821a9ed39360a805cfa7164c1
Feda Curic <feda.curic@gmail.com>
2020-12-12 16:10:04 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -6,5 +6,13 @@ with open("fetcha/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
-    name="fetcha", version=version, packages=find_packages(), include_package_data=True
+    name="fetcha", 
+    version=version, 
+    description="Talk to SSB using Python.", 
+    url="https://github.com/dafeda/fetcha",
+    author="Feda Curic",
+    author_email="feda.curic@gmail.com",
+    license="?",
+    packages=find_packages(), 
+    include_package_data=True
 )
