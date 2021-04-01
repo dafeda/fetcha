72e9332e24d5486023fe7be4bc9b5c8948d4b71b
c624fc0 <feda.curic@spv.no>
2021-04-01 09:57:52 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ from setuptools import setup, find_packages
 with open("fetcha/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="fetcha",
     version=version,
     description="Talk to SSB using Python.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',    
     url="https://github.com/dafeda/fetcha",
     author="Feda Curic",
     author_email="feda.curic@gmail.com",
